b8d7ca3409b300b88a0d8f0e71facda43ae3dcb3
Krzysztof Hager <hager.krzysztof.hk@gmail.com>
2024-06-22 16:04:33 +0200

    Obsługa homepage, przechodzenie pomiędzy views

--- project/usersAPP/urls.py
+++ project/usersAPP/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="users")
+    path('', views.user_home, name="home"),
+    path('signin', views.login_view, name="signin"),
+    path('signup', views.signup_user, name="signup"),
+    path('joinquiz', views.join_quiz, name="joinquiz")
 ]
--- project/usersAPP/views.py
+++ project/usersAPP/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
-def index(request):
-    return render(request, 'users/index.html')
+def user_home(request):
+    return render(request, 'users/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST["username"],
+                            password=request.POST["password"])
+        if user:
+            login(request, user)
+            messages.success(request, 'Logged in successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Logged in Fail')
+    return render(request, 'users/signin.html')
+
+def signup_user(request):
+    return render(request, 'users/signup.html')
+
+def join_quiz(request):
+    return render(request, 'users/joinquiz.html')
