ab2a81c1dbf676363f85bb5ead70475055d5d963
Eli <dsw52678@student.dsw.edu.pl>
2024-06-23 21:06:46 +0200

    added working listing of quizes and their questions

--- project/quizAPP/urls.py
+++ project/quizAPP/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("",views.home, name='home'),
     path('create/', views.create_quiz, name='quizwizard'), 
-    path('quizlist/', views.quiz_list, name='quizlist')
+    path('quizlist/', views.quiz_list, name='quizlist'),
+    path('quizzes/<int:quiz_id>/', views.question_list, name='questionlist')
 ]
 
--- project/quizAPP/views.py
+++ project/quizAPP/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Quiz
+from .forms import QuizForm
 
 # Create your views here.
 
@@ -7,8 +9,21 @@ basepath = "..\\templates\\quiz\\"
 def home(request):
     return render(request, f'{basepath}home.html')
 
+
+
 def create_quiz(request):
+    if request.method == 'POST':
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            quiz.created_by = request.user
+            quiz.save()
+            #return redirect('quiz_list')  # Redirect to a view that lists quizzes
+        else:
+            form = QuizForm()
+
     return render(request, f'{basepath}quizwizard.html')
+
 
 def add_question(request):
     pass
@@ -16,10 +31,9 @@ def add_question(request):
 
 
 def quiz_list(request):
-    # to be implemented after adding working functionality for quizzes
-
-    """
     quizzes = Quiz.objects.all()  # Replace Quiz with your actual Quiz model - to be added once 
-    return render(request, 'quiz_list.html', {'quizzes': quizzes})  # Replace with your actual template for quiz list
-    """ 
-    return render(request,  f'{basepath}quizlist.html')
+    return render(request, f'{basepath}quizlist.html', {'quizzes': quizzes})  # Replace with your actual template for quiz list
+
+def question_list(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    return render(request, 'quiz/questionlist.html', {'quiz': quiz})
