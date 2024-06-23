196fbc23e4825f025d347d67764cda5561bfea88
Eli <dsw52678@student.dsw.edu.pl>
2024-06-23 15:41:27 +0200

    Migrated example db's. Added testquiz with 2 questions from admin panel.

--- project/quizAPP/models.py
+++ project/quizAPP/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    #created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='quizzes')      #Placeholder, to be replaced with Users handle
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='quizzes')
     
     def __str__(self):
         return self.title
