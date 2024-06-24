b6ba115a6e60a5e4f6f8ea93c65b84741ae6cfdf
Krzysztof Hager <hager.krzysztof.hk@gmail.com>
2024-06-24 23:09:46 +0200

    Podpięcie wszystkich widoków i odnośników

--- project/homeAPP/urls.py
+++ project/homeAPP/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainpage, name="home")
+    path('', views.mainpage, name="mainpage"),
+    path('join', views.main_join, name="join"),
+    path('aboutus', views.aboutus, name="aboutus"),
+    path('termsofuse', views.terms, name="terms"),
+    path('contact', views.contact, name="contact")
 ]
