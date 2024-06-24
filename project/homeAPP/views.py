5da698273c185a0275853924ffa78417e7d23f43
Krzysztof Hager <hager.krzysztof.hk@gmail.com>
2024-06-24 23:09:46 +0200

    Podpięcie wszystkich widoków i odnośników

--- project/homeAPP/views.py
+++ project/homeAPP/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def mainpage(request):
     return render(request, 'home/mainpage.html')
+
+def main_join(request):
+    return render(request, "home/join.html")
+
+def aboutus(request):
+    return render(request, "home/aboutus.html")
+
+def terms(request):
+    return render(request, "home/terms.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
