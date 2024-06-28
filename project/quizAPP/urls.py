1cb572200d9fd19023c470a5df37d0961b147f9b
Eli <dsw52678@student.dsw.edu.pl>
2024-06-28 19:23:06 +0200

    added quiz listing under 'Rozpocznij nowy quiz'

--- project/quizAPP/urls.py
+++ project/quizAPP/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.quizhome, name='quizhome'),
     path('create/', views.create_quiz, name='quizwizard'), 
     path('quizlist/', views.quiz_list, name='quizlist'),
-    path('quizzes/<int:quiz_id>/', views.question_list, name='questionlist')
+    path('quizzes/<int:quiz_id>/', views.question_list, name='questionlist'),
+    path('testlist/',views.test_list,name='testlist')
 ]
