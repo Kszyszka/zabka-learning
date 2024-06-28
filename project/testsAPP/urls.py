1821dbc56d9cf958bc194675f011e351b01d0c94
Kacper <kgrzesik17@gmail.com>
2024-06-28 19:13:56 +0200

    dodano przykladowa strone domowa dla aplikacji do wykonywania testow

--- project/testsAPP/urls.py
+++ project/testsAPP/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("attend", views.attend, name='attend')
+    path("", views.attend, name='attend')
 ]
--- project/testsAPP/views.py
+++ project/testsAPP/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def attend(request):    
-    return render(request, 'attend.html')
+    return render(request, 'attend/home.html')
