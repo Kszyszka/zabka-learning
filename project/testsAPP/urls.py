a46a5b20b580759797fd185516024295fcd23bd7
Kacper <kgrzesik17@gmail.com>
2024-06-28 19:49:13 +0200

    dodano podstawowe wyswietlanie quizow w aplikacji testAPP

--- project/testsAPP/urls.py
+++ project/testsAPP/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.attend, name='attend')
+    path("", views.attend, name='attend'),
+    path('attend/<int:quiz_id>/', views.attend_quiz, name='attend_quiz')
 ]
