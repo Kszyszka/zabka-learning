7e8a2b7155e225c946236cfea413b5d02870ea07
Kacper <kgrzesik17@gmail.com>
2024-06-28 20:29:13 +0200

    dodanie przekierowania do odpowiedniej strony po wypelnieniu quizu w testapp

--- project/testsAPP/views.py
+++ project/testsAPP/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from quizAPP.models import *
 
-def attend(request):
-    quizzes = Quiz.objects.all()
-    context = {'quizzes': quizzes}
-
-    current_user = request.user
-
-    return render(request, 'attend/home.html', context)
-
 def attend_quiz(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     return render(request, 'attend/attendquiz.html', {'quiz': quiz})
+
+def results(request):
+    return render(request, 'attend/results.html')
